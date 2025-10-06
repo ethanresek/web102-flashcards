8711d5b50e1ea874482cabcba110bd0645d5cf1e
ethanresek <resekethan@gmail.com>
2025-10-06 18:48:52 -0400

    Completed README

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ function App() {
 
   const values = [
   { question: 'Start!', answer: 'Press next!' },
-  { question: '1', answer: '一' },
-  { question: '2', answer: '二' },
-  { question: '3', answer: '三' },
-  { question: '4', answer: '四' },
-  { question: '5', answer: '五' },
-  { question: '6', answer: '六' },
-  { question: '7', answer: '七' },
-  { question: '8', answer: '八' },
-  { question: '9', answer: '九' },
-  { question: '10', answer: '十' }
+  { question: '一', answer: '1' },
+  { question: '二', answer: '2' },
+  { question: '三', answer: '3' },
+  { question: '四', answer: '4' },
+  { question: '五', answer: '5' },
+  { question: '六', answer: '6' },
+  { question: '七', answer: '7' },
+  { question: '八', answer: '8' },
+  { question: '九', answer: '9' },
+  { question: '十', answer: '10' }
 ]
 
   const shuffle = (array) => {
@@ -36,27 +36,37 @@ function App() {
     return array;
   }
 
-  const [index, setIndex] = useState(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+  const [indexArray, setIndexArray] = useState(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
   const [cardNum, setCardNum] = useState(0);
   const [flipped, setFlipped] = useState('card');
-  
-  const getIndex = () => {
-    let i = Math.floor(Math.random() * index.length);
-    let returnVal = index[i];
-    const newIndex = [...index];
-    newIndex.splice(i, 1);
-    setIndex(newIndex);
-    return returnVal;
-  }
+  const [index, setIndex] = useState(-1);
+  const [input, setInput] = useState('');
+  const [inputStyle, setInputStyle] = useState('guess-input');
   
   const clickNext = () => {
     setFlipped('card');
+    setInputStyle('guess-input');
+    setInput('');
     setTimeout(() => {
-      if (index.length > 0) {
-        setCardNum(getIndex());
-      }
+      setIndex(prevIndex => {
+        const newIndex = prevIndex + 1;
+        setCardNum(indexArray[newIndex]);
+        return newIndex;
+      });
     }, 180)
-    
+  };
+
+  const clickBack = () => {
+    setFlipped('card');
+    setInputStyle('guess-input');
+    setInput('');
+    setTimeout(() => {
+      setIndex(prevIndex => {
+        const newIndex = prevIndex - 1;
+        setCardNum(indexArray[newIndex]);
+        return newIndex;
+      });
+    }, 180)
   };
 
   const clickCard = () => {
@@ -67,7 +77,23 @@ function App() {
     } else {
       setFlipped('card flipped');
     }
-  };  
+  };
+
+  const saveInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const checkInput = () => {
+    if (input.length == 0) {
+      setInputStyle('guess-input')
+    } else if (input == values[cardNum].answer) {
+      setInputStyle('guess-input correct')
+    } else if (input != values[cardNum].answer) {
+      setInputStyle('guess-input incorrect')
+    }
+  }
+
+
 
   return (
     <div className='card-app'>
@@ -82,7 +108,14 @@ function App() {
       <div className='outer-card'>
         <Card flipped={flipped} cardNum={cardNum} card={values} flipFunction={clickCard}/> 
       </div>
-      <button className="next" onClick={clickNext}>Next</button>
+      <div className='input-box'>
+        <input className={inputStyle} type="text" value={input} onChange={saveInput} placeholder='Write your guess here!' disabled={index < 0}/>
+        <button className='input-button' onClick={checkInput}>Submit</button>
+      </div>
+      <div className='button-box'>
+        <button className="backButton" onClick={clickBack} disabled={index <= 0}>Back</button>
+        <button className="nextButton" onClick={clickNext} disabled={index >= indexArray.length - 1}>Next</button>
+      </div>
     </div>
   )
 };
